4913051c8bb3bb38dea1beeee20bafbbba25f90a
Jeff Fischer <jeff@data-ken.org>
2016-06-30 20:57:32 -0700

    Main script for lighting sensors

---
+++ examples/lighting_replay/esp8266_main.py
@@ -0,0 +1,27 @@
+"""This runs in Micropython on the esp8266. It samples the
+lux sensor and pushes to an mqtt queue.
+"""
+from antevents import Scheduler 
+from tsl2591 import Tsl2591
+from mqtt_writer import MQTTWriter
+from wifi import wifi_connect
+import os
+
+# Params to set
+WIFI_SID=""
+WIFI_PW=""
+SENSOR_ID="lux-1"
+BROKER='192.168.11.153'
+
+wifi_connect(WIFI_SID, WIFI_PW)
+sensor = Tsl2591()
+writer = MQTTWriter(SENSOR_ID, BROKER, 1883, BROKER)
+sched = Scheduler()
+sched.schedule_sensor(sensor, SENSOR_ID, 60, writer)
+try:
+  os.stat('stop-sampling.txt')
+  print("Saw stop-sampling.txt, will skip rest")
+except:
+  print("Starting sensor sampling")
+  sched.run_forever()
+
