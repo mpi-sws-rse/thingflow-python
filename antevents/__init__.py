2280624b54ec8f1ebae656336fab13d032f504ad
Jeff Fischer <jeff@data-ken.org>
2016-09-16 09:51:20 -0700

    Update doc string on location of sensors

--- antevents/__init__.py
+++ antevents/__init__.py
@@ -2,16 +2,16 @@
 # Licensed under the Apache 2.0 License.
 """
 This is the main package for antevents. Directly within this package you fill
-find the following modules:
+find the following module:
 
  * `base` - the core abstractions and classes of the system.
- * `sensor` - defines data types and functions specifically for sensor events.
 
 The rest of the functionality is in sub-packages:
 
  * `adapters` - components to read/write events outside the system
  * `internal` - some internal definitions
  * `linq` - filters that allow linq-style query pipelines over event streams
+ * `sensors` - interfaces to sensors go here
 """
 
 __version__ = "1.0"
