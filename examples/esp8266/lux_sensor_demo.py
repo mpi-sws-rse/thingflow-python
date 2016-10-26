33aef9b00f6461ee08c86884cbcae02b7d5828b5
Jeff Fischer <jeff@data-ken.org>
2016-10-25 18:47:04 -0700

    Added reference to examples dir; lux sensor demo on 8266

---
+++ examples/esp8266/lux_sensor_demo.py
@@ -0,0 +1,19 @@
+# Simple demo of reading the tsl2591 lux sensor from the
+# ESP8266 running micropython.
+
+from antevents import *
+from tsl2591 import Tsl2591
+tsl = Tsl2591('lux-1')
+tsl.sample()
+sched = Scheduler()
+
+class Output:
+    def on_next(self, x):
+        print(x)
+    def on_completed():
+        pass
+    def on_error(self, e):
+        pass
+
+sched.schedule_sensor(tsl, 2.0, Output()
+sched.run_forever()
