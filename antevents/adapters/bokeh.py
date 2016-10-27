953f82a8d73b0deeaf509e322f37faec7a520cb1
Rupak Majumdar <rupak@mpi-sws.org>
2016-10-27 14:01:43 +0200

    Adding Bokeh Manager functionality.

--- antevents/adapters/bokeh.py
+++ antevents/adapters/bokeh.py
@@ -18,7 +18,7 @@ from bokeh.charts import TimeSeries, show, output_file, output_server
 from bokeh.plotting import figure, curdoc
 from bokeh.layouts import column # to show two or more plots arranged in a column
 import numpy as np
-from bokeh.models import ColumnDataSource, Slider
+from bokeh.models import ColumnDataSource
 from bokeh.client import push_session
 from bokeh.driving import linear
 
@@ -64,14 +64,97 @@ def bokeh_output(csv, mapper=bokeh_timeseries_mapper):
     #                     title="Sensor", tools=TOOLS, ylabel='Sensor readings', xlabel='Time') 
     # show(tsline)
 
-source = ColumnDataSource(dict(timestamp=[], value=[]))
+
+class BokehPlotWorker(threading.Thread):
+    def __init__(self, plotters):
+        threading.Thread.__init__(self)
+        self.plotters = plotters
+
+    def update(self, whichq, whichsource):
+        print("In update")
+        try:
+            data = whichq.get_nowait()
+            if data:
+                print('data = ', data)
+                ts = data.ts
+                val = data.val
+                new_data = dict(timestamp=[ts], value=[val]) 
+                whichsource.stream(new_data, 300)
+        except queue.Empty:
+            pass
+
+    def make_fig(self, plot_source):
+        plot_specs = plot_source['plot_specs']
+        p = figure(plot_height=500, tools=TOOLS, y_axis_location='left', title=plot_specs.name)
+        p.x_range.follow = "end"
+        p.xaxis.axis_label = plot_specs.x_axis_label 
+        p.yaxis.axis_label = plot_specs.y_axis_label 
+        self.p.x_range.follow_interval = 100
+        self.p.x_range.range_padding = 0 
+        self.p.line(x=plot_specs.x_axis_label, y=plot_specs.y_axis_label, color="blue", source=plot_specs.source)
+        self.p.circle(x=plot_specs.x_axis_label, y=plot_specs.y_axis_label, color="red", source=plot_specs.source)
+        curdoc().add_periodic_callback(functools.partial(self.update, whichqueue=plot_source['queue'], whichsource=plot_specs.source), plot_specs.update_period) #period in ms
+
+    def run(self):
+        print("In thread.run")
+        self.figs = map(self.make_fig, list(self.plotters))
+        
+        self.session = push_session(curdoc())
+        self.session.show(column(self.figs)) 
+        curdoc().title = 'AntEvent Streams' 
+        self.session.loop_until_closed()
+
+
+
+class BokehPlot(object):
+    def __init__(self, name, y_axis_label="", x_axis_label="Time", update_period_in_ms=500):
+        self.name = name
+        self.x_axis_label = x_axis_label
+        self.y_axis_label = y_axis_label
+        self.update_period = update_period_in_ms
+        self.source = ColumnDataSource(dict(x_axis_label=[], y_axis_label=[]))
+
+class BokehPlotManager(object):
+    def __init__(self):
+        self.plots = { }
+        self.open_for_registration = True
+        self.started = False
+
+    def register(self, plot):
+        if self.open_for_registration: 
+            self.plots[plot.name] = { 'queue' : queue.Queue(), 'plot_specs' : plot }
+        else:
+            raise Exception("Bokeh Adapter: Plot manager does not dynamically add registrations.")
+
+    def start(self):
+        self.open_for_registration = False
+        self.bokeh_plot_worker = BokehPlotWorker(self.plots)
+        self.bokeh_plot_worker.start()
+        self.started = True
+
+
+    def on_next(self, t): 
+        whichplot, data = t
+        assert self.started, "BokehPlotManager: Data sent without initialization"
+        if whichplot in self.plots:
+            self.plots[whichplot]['queue'].put(data)
+        else:
+            raise Exception("Plot %s not found among registered plots", whichplot)
+
+    def on_completed(self):
+        pass
+
+    def on_error(self):
+        pass
 
 class BokehOutputWorker(threading.Thread):
+    source = ColumnDataSource(dict(timestamp=[], value=[]))
     def __init__(self, sensor_id, datasource):
         threading.Thread.__init__(self)
         self.q = datasource
-        self.counter = 0
         self.title = sensor_id
+
+        self.counter = 0
 
     def update(self):
         print("In update")
@@ -82,7 +165,7 @@ class BokehOutputWorker(threading.Thread):
                 ts = data.ts
                 val = data.val
                 new_data = dict(timestamp=[ts], value=[val]) 
-                source.stream(new_data, 300)
+                self.source.stream(new_data, 300)
                 self.counter = 0
         except queue.Empty:
             pass
@@ -93,30 +176,30 @@ class BokehOutputWorker(threading.Thread):
     def run(self):
         print("In thread.run")
         self.p = figure(plot_height=500, tools=TOOLS, y_axis_location='left', title=self.title)
-        # self.mean = Slider(title="mean", value=0, start=-0.01, end=0.01, step=0.001)
         self.p.x_range.follow = "end"
         self.p.xaxis.axis_label = "Timestamp"
         self.p.x_range.follow_interval = 100
         self.p.x_range.range_padding = 0 
-        self.p.line(x="timestamp", y="value", color="blue", source=source)
-        self.p.circle(x="timestamp", y="value", color="red", source=source)
+        self.p.line(x="timestamp", y="value", color="blue", source=self.source)
+        self.p.circle(x="timestamp", y="value", color="red", source=self.source)
 
-        session = push_session(curdoc())
+        self.session = push_session(curdoc())
         curdoc().add_periodic_callback(self.update, 100) #period in ms
 
-        session.show(column(self.p)) 
+        self.session.show(column(self.p)) 
         curdoc().title = 'Sensor' 
-        session.loop_until_closed()
- 
+        self.session.loop_until_closed()
+
+    # def register(self, d, sourceq):
+    #     source = ColumnDataSource(dict(d))
+    #     self.p.line(x=d[0], y=d[1], color="orange", source=source)
+    #     curdoc().add_periodic_callback(self.update, 100) #period in ms
 
 class BokehStreamer(Filter):
-    def __init__(self, initial_csv, mapper=bokeh_timeseries_mapper, io_loop=None):
+    def __init__(self, initial_csv, io_loop=None):
         self.q = queue.Queue() 
-        self.mapper = mapper
         self.bokeh_worker = BokehOutputWorker("Sensor", self.q)
         self.bokeh_worker.start()
-
- 
 
     def on_next(self, x):
         print("next:", x)
@@ -131,8 +214,10 @@ class BokehStreamer(Filter):
         self.q.join()
         self._dispatch_error(e)
 
+
 def bokeh_output_streaming(csv):
     """Write an event stream to a Bokeh visualizer
     """    
     b = BokehStreamer(csv)
+
 
--- tests/test_bokeh.py
+++ tests/test_bokeh.py
@@ -2,7 +2,8 @@ import asyncio
 from utils import ValueListSensor, ValidationSubscriber
 from antevents.base import Scheduler, SensorPub, SensorEvent
 
-from antevents.adapters.bokeh import bokeh_output, bokeh_output_streaming, BokehStreamer
+from antevents.linq.map import map
+from antevents.adapters.bokeh import bokeh_output, bokeh_output_streaming, BokehStreamer, BokehPlot, BokehPlotManager
 import datetime, time
 
 def mk_csv():
@@ -23,6 +24,7 @@ def debug():
     bokeh_output_streaming(csv)
 
 value_stream = [10, 13, 20, 20, 19, 19, 20, 21, 28, 28, 23, 21, 21, 18, 19, 16, 21]
+value_stream2 = [2, 3, 2, 2, 9, 9, 2, 1, 8, 8, 3, 2, 1, 8, 9, 6, 2]
 
 def test_bokeh_output():
     loop = asyncio.get_event_loop()
@@ -38,7 +40,30 @@ def test_bokeh_output():
         "Schedule exited before validation observer completed")
     print("That's all folks")
 
+def test_bokeh_manager():
+    loop = asyncio.get_event_loop()
+    s1 = ValueListSensor(1, value_stream)
+    p1 = SensorPub(s1)
+    s2 = ValueListSensor(1, value_stream2)
+    p2 = SensorPub(s2)
 
+    bm = BokehPlotManager()
+    bplot1 = BokehPlot('Sensor1', y_axis_label='s1')  
+    bplot2 = BokehPlot('Sensor2', y_axis_label='s1')  
+    bm.register(bplot1)
+    bm.register(bplot2)
+    p1.map(lambda v: ('Sensor1', v)  ).subscribe(bm)
+    p2.map(lambda v: ('Sensor2', v)  ).subscribe(bm)
+    bm.start()
+ 
+    scheduler = Scheduler(loop)
+    scheduler.schedule_periodic(p1, 0.5) # sample twice every second
+    scheduler.schedule_periodic(p2, 0.5) # sample twice every second
+    scheduler.run_forever()
+    # self.assertTrue(vo.completed,
+    #     "Schedule exited before validation observer completed")
+    print("That's all folks")
+    
 if __name__ == "__main__":
-    #debug()
-    test_bokeh_output()
+    # test_bokeh_output()
+    test_bokeh_manager()
