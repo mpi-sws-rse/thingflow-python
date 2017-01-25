883c5463a533330cf5d92ddf2274cbbbfb9394c9
Jeff Fischer <jeff@data-ken.org>
2017-01-24 21:32:51 -0800

    Fixes jfischer/micropython-iot-hackathon#9 - we give an message when waiting for the connection

--- micropython/wifi.py
+++ micropython/wifi.py
@@ -1,4 +1,5 @@
 import network
+import utime
 
 def wifi_connect(essid, password):
     # Connect to the wifi. Based on the example in the micropython
@@ -6,11 +7,19 @@ def wifi_connect(essid, password):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     if not wlan.isconnected():
-        print('connecting to network...')
+        print('connecting to network ' + essid + '...')
         wlan.connect(essid, password)
+        # connect() appears to be async - waiting for it to complete
         while not wlan.isconnected():
-            pass
-    print('network config: %s' % repr(wlan.ifconfig()))
+            print('waiting for connection...')
+            utime.sleep(4)
+            print('checking connection...')
+        print('Wifi connect successful, network config: %s' % repr(wlan.ifconfig()))
+    else:
+        # Note that connection info is stored in non-volatile memory. If
+        # you are connected to the wrong network, do an explicity disconnect()
+        # and then reconnect.
+        print('Wifi already connected, network config: %s' % repr(wlan.ifconfig()))
 
 
 def disable_wifi_ap():
